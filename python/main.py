d11999eb26cfe68ac9b7578bf3085aede1131f5d
sergioctf <sctcarmelo@gmail.com>
2024-04-17 22:51:40 +0000

    uma cremaaa

--- python/main.py
+++ python/main.py
@@ -49,87 +49,43 @@ botao_letra = {
     4: 'l'    # Laranja
 }
 
-cases_press = {
-            8: {'a'},
-            16: {'s'},
-            2: {'j'},
-            1: {'k'},
-            4: {'l'},
-            24: {'a', 's'},
-            10: {'a', 'j'},
-            9: {'a', 'k'},
-            12: {'a', 'l'},
-            18: {'s', 'j'},
-            17: {'s', 'k'},
-            20: {'s', 'l'},
-            3: {'j', 'k'},
-            6: {'j', 'l'},
-            5: {'k', 'l'},
-            30: {'a', 's', 'j'},
-            28: {'a', 's', 'k'},
-            22: {'a', 'j', 'k'},
-            14: {'a', 'j', 'l'},
-            13: {'a', 'k', 'l'},
-            7: {'s', 'j', 'k'},
-            11: {'s', 'j', 'l'},
-            19: {'s', 'k', 'l'},
-            15: {'j', 'k', 'l'},
-            31: {'a', 's', 'j', 'k'},
-            29: {'a', 's', 'j', 'l'},
-            27: {'a', 's', 'k', 'l'},
-            23: {'a', 'j', 'k', 'l'},
-            26: {'s', 'j', 'k', 'l'},
-            25: {'a', 's', 'j', 'k', 'l'},
-        }
-cases_release = {
-            8: {'s','j','k','l'},
-            16: {'a','j','k','l'},
-            2: {'a','s','k','l'},
-            1: {'a','s','j','l'},
-            4: {'a','s','j','k'},
-            24: {'j','k','l'},
-            10: {'s','k','l'},
-            9: {'s','j','l'},
-            12: {'s','j','k'},
-            18: {'a','k','l'},
-            17: {'a','j','l'},
-            20: {'a','j','k'},
-            3: {'a','s','l'},
-            6: {'a','s','j'},
-            5: {'a','s','k'},
-            30: {'s'},
-            28: {'j'},
-            22: {'k'},
-            14: {'l'},
-            13: {'a'},
-            7: {'s','j','k','l'},
-            11: {'a','j','k','l'},
-            19: {'a','s','k','l'},
-            15: {'a','s','j','l'},
-            31: set(),  # Não há necessidade de liberar nenhuma tecla
-            29: set(),  # Não há necessidade de liberar nenhuma tecla
-            27: set(),  # Não há necessidade de liberar nenhuma tecla
-            23: set(),  # Não há necessidade de liberar nenhuma tecla
-            26: set(),  # Não há necessidade de liberar nenhuma tecla
-            25: set(),  # Não há necessidade de liberar nenhuma tecla
-        }
 
 def levantar_tecla(button_index):
-    if button_index in cases_release:
-        for key in cases_release[button_index]:
-                keyboard.release(key)    
-    else:
-        pass
-def press_key(button_index):
-    if button_index in cases_press: 
-        for key in cases_press[button_index]:
-            keyboard.press(key)
-        levantar_tecla(button_index)
-    else:
-        pass
+    # Convertendo o índice do botão para binário
+    bin_button = bin(button_index)[2:].zfill(5)  # Garante que tenhamos 5 dígitos binários
+
+    # Iterando sobre cada bit para verificar e levantar as teclas necessárias
+    for i, bit in enumerate(bin_button[::-1]):  # Inverte a string binária para começar do bit menos significativo
+        if bit == '1':
+            keyboard.release(botao_letra[i])
+            
 
 
-botao_anterior = 0
+def press_key(binary_number):
+    for index, bit in enumerate(binary_number):
+        if bit == '1':
+            if index == 0:
+                keyboard.press('s')
+            elif index == 1:
+                keyboard.press('a')
+            elif index == 2:
+                keyboard.press('l')
+            elif index == 3:
+                keyboard.press('j')
+            elif index == 4:
+                keyboard.press('k')
+        elif bit == '0':
+            if index == 0:
+                keyboard.release('s')
+            elif index == 1:
+                keyboard.release('a')
+            elif index == 2:
+                keyboard.release('l')
+            elif index == 3:
+                keyboard.release('j')
+            elif index == 4:
+                keyboard.release('k')
+
 try:
     while True:
         # Lê os dados da porta serial
@@ -156,12 +112,11 @@ try:
         print("\n\n")
         move_mouse(axis, valor_mouse)
 
-        if data_button:
-            #passando data_bubtton pra binario
-            if data_button != botao_anterior:
-                levantar_tecla(botao_anterior)
-                botao_anterior = data_button
-            press_key(data_button)
+        if data_button is not None:
+            # transofrmando data_button em um binario que necessarioamente tem que ter 5 bits
+            d = bin(data_button)[2:].zfill(5)
+            print(d)
+            press_key(d)
 
 except KeyboardInterrupt:
     print("Program terminated by user")
@@ -169,3 +124,4 @@ except Exception as e:
     print(f"An error occurred: {e}")
 finally:
     ser.close()
+
